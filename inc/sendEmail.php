ef51ce248184ffd700a4397e342f89e6a34ee510
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2020-05-14 14:28:06 -0500

    Edit sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -4,7 +4,7 @@
 $siteOwnersEmail = 'gabrieltomberlin14@gmail.com';
 
 
-if($_POST) {
+if($_SERVER['REQUEST_METHOD'] == "POST") {
 
    $name = trim(stripslashes($_POST['contactName']));
    $email = trim(stripslashes($_POST['contactEmail']));
